793c994684b479f3a09546a413a75bb1134fd778
Andrea Telatin <andrea@telatin.com>
2020-03-16 20:12:35 +0000

    test udpate

--- test/all.sh
+++ test/all.sh
@@ -3,50 +3,61 @@ TEST_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 SCRIPTS="$TEST_DIR/../bin"
 INPUT="$TEST_DIR/../data/test.fa"
 
+echo -e "conda_prefix:\t$CONDA_PREFIX"
+echo -e "conda_prompt:\t$CONDA_PROMPT_MODIFIER"
+echo -e "current_path:\t$PWD"
+echo -e "list_binaries:\n"$(ls "$SCRIPTS")
+echo ''
+
 echo -ne "[fq-len]\tcheck minimum length:\t"
-MIN_LEN=$($SCRIPTS/fu-len.pl -m 100 $INPUT | wc -l)
+MIN_LEN=$($SCRIPTS/fu-len.pl -m 100 $INPUT 2>/dev/null | wc -l)
 if [[ $MIN_LEN -eq 8 ]]; then
 	echo "PASS"
 else
 	echo "FAIL ($MIN_LEN != 8)"
-	exit
+	exit 1
 fi
+echo ""
 
 echo -ne "[fq-len]\tmin and max length:\t"
-MINMAX=$($SCRIPTS/fu-len.pl -m 10 -x 150 $INPUT | wc -l)
+MINMAX=$($SCRIPTS/fu-len.pl -m 10 -x 150 $INPUT 2>/dev/null| wc -l)
 if [[ $MINMAX -eq 2 ]]; then
 	echo "PASS"
 else
 	echo "FAIL ($MINMAX != 2)"
-	exit
+	exit 1
 fi
+echo ""
 
 echo -ne "[fq-grep]\tcheck forward match:\t"
-GREP1=$($SCRIPTS/fu-grep.pl   CCCC $INPUT |wc -l )                                                      
+GREP1=$($SCRIPTS/fu-grep.pl   CCCC $INPUT 2>/dev/null|wc -l )                                                      
 if [[ $GREP1 -eq 8 ]]; then
 	echo "PASS"
 else
 	echo "FAIL ($GREP1 != 8)"
-	exit
+	exit 1
 fi
+echo ""
 
 echo -ne "[fq-grep]\tcheck reverse compl.:\t"
-GREP2=$($SCRIPTS/fu-grep.pl   GGGG $INPUT |wc -l )                                                      
+GREP2=$($SCRIPTS/fu-grep.pl   GGGG $INPUT 2>/dev/null|wc -l )                                                      
 if [[ $GREP2 -eq 8 ]]; then
 	echo "PASS"
 else
 	echo "FAIL ($GREP2 != 8)"
-	exit
+	exit 1 
 fi
+echo ""
 
 echo -ne "[fq-rename]\tcheck prefix rename:\t"
-RENAME=$($SCRIPTS/fu-rename.pl $INPUT -p new_prefix | grep new_prefix | wc -l)                                                      
+RENAME=$($SCRIPTS/fu-rename.pl $INPUT -p new_prefix 2>/dev/null| grep new_prefix | wc -l)                                                      
 if [[ $RENAME -eq 8 ]]; then
 	echo "PASS"
 else
 	echo "FAIL ($RENAME != 8)"
-	exit
+	exit 1
 fi
+echo ""
 
 # Conda installed:
 n50 $INPUT
